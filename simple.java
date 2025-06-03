37cd5e250e9ddce0ed7c6bfb022904d8a425410a
pruthvesh <pruthvesh653@gmail.com>
2025-06-03 00:09:43 -0400

    Update simple.java

--- simple.java
+++ simple.java
@@ -1,5 +1,17 @@
+// Importing an external library
+import org.apache.commons.lang3.StringUtils;  
+
+// Importing built-in Java utilities
+import java.util.Scanner;
+
 public class simple {
     public static void main(String[] args) {
         System.out.println("Hello, Jenkins CI/CD!");
+
+        // Example usage of imported Scanner class
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String name = scanner.nextLine();
+        System.out.println("Welcome, " + name + "!");
     }
 }
